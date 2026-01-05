f10911dacf4383c01a743d6c0131f8e2dd12c7e1
ikari0515-cmyk <ikari0515@gmail.com>
2026-01-05 22:31:17 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-
   async redirects() {
-  
-  return [
-    {
-      source: "/",
-      has: [{ type: "host", value: "asteriskjob.com" }],
-      destination: "https://job.asteriskjob.com",
-      permanent: true,
-    },
-  ];
-},
-}
+    return [
+      {
+        source: "/",
+        has: [{ type: "host", value: "asteriskjob.com" }],
+        destination: "https://job.asteriskjob.com",
+        permanent: true,
+      },
+      {
+        source: "/",
+        has: [{ type: "host", value: "www.asteriskjob.com" }],
+        destination: "https://job.asteriskjob.com",
+        permanent: true,
+      },
+    ];
+  },
+};
+
 export default nextConfig;
